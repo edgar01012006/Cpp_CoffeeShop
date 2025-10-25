8e9f7dc480f07dfd389715bd61178d7977d35783
edgar01012006 <edgarsalardzorcyan2006@gmail.com>
2025-10-25 19:47:41 +0400

    deleting old unnecassary files

--- Drink.cpp
+++
@@ -1,7 +0,0 @@
-#include "Drink.hpp"
-#include <iostream>
-
-void Drink::printDrink() const {
-    std::cout << "Name: " << m_name << std::endl;
-    std::cout << "Price: " << m_price << std::endl;
-}
